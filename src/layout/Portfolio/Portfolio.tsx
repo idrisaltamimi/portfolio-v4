fcf63fb56e63004133fb5999a25339ad6bd2fbd9
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-06-02 06:45:18 +0400

    add projects in Portfolio section

--- src/layout/Portfolio/Portfolio.tsx
+++ src/layout/Portfolio/Portfolio.tsx
@@ -1,59 +1,52 @@
-import { type ReactNode } from "react"
-
 import { Section } from "../../components"
-import gptOmni from "../../assets/gpt-omni.svg"
-import kanban from "../../assets/kanban.svg"
+import { PortfolioSubSection, PortfolioCard } from "./"
+import { gptOmni, kanban, gptOmniIcon, kanbanIcon } from "../../assets"
 import "./Portfolio.scss"
 
 export default function Portfolio() {
   return (
-    <Section className="portfolio">
-      <h2>A selection of my best work</h2>
+    <Section className="portfolio" id="portfolio">
+      <div className="section--wrapper">
+        <h2>My Projects</h2>
+        <span className="portfolio--subtitle">
+          Here a selection of projects I've worked on.
+        </span>
 
-      <PortfolioSubSection title="My Apps">
-        <PortfolioCard
-          name="GPT-OMNI"
-          image={gptOmni}
-          techStack={["React", "Typescript", "TailwindCSS", "Nodejs", "OpenAi"]}
-        />
-        <PortfolioCard name="Kanban" image={kanban} techStack={[]} />
-      </PortfolioSubSection>
-      <h3>My Packages</h3>
-      <h3>Cloned Websites</h3>
+        <PortfolioSubSection title="My Apps">
+          <PortfolioCard
+            name="GPT-OMNI"
+            image={gptOmni}
+            logo={gptOmniIcon}
+            techStack={[
+              "React",
+              "Typescript",
+              "TailwindCSS",
+              "Nodejs",
+              "OpenAi"
+            ]}
+          />
+          <PortfolioCard
+            name="Kanban"
+            image={kanban}
+            logo={kanbanIcon}
+            techStack={[
+              "React",
+              "Typescript",
+              "CSS",
+              "Nodejs",
+              "Express",
+              "MongoDB"
+            ]}
+          />
+        </PortfolioSubSection>
+        <h3>My Packages</h3>
+        <h3>Cloned Websites</h3>
+      </div>
+
+      <div className="portfolio--overlay-text overlay-text">Projects</div>
+      {pattern1Svg}
     </Section>
   )
 }
 
-type PortfolioSubSectionProps = {
-  title: string
-  children: ReactNode
-}
-
-function PortfolioSubSection({ title, children }: PortfolioSubSectionProps) {
-  return (
-    <div className="portfolio--subsection">
-      <h3>{title}</h3>
-      <ul>{children}</ul>
-    </div>
-  )
-}
-
-type PortfolioCardProps = {
-  name: string
-  image: string
-  techStack: Array<string>
-}
-
-function PortfolioCard({ name, image, techStack }: PortfolioCardProps) {
-  return (
-    <li className="portfolio--card">
-      <span>{name}</span>
-      <img src={image} alt="" width={200} />
-      <ul>
-        {techStack.map((tech) => {
-          return <li key={tech}>{tech}</li>
-        })}
-      </ul>
-    </li>
-  )
-}
+const pattern1Svg = ""
