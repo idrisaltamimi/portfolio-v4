3b129ae68ff93c6321974081a2924f31b6b075dc
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-06-02 06:43:42 +0400

    add some general styles

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import gptOmni from "./portfolioImages/gpt-omni.png"
+import kanban from "./portfolioImages/kanban.png"
+import gptOmniIcon from "./portfolioImages/gpt-omni-icon.svg"
+import kanbanIcon from "./portfolioImages/kanban-icon.svg"
+
+export { gptOmni, kanban, gptOmniIcon, kanbanIcon }
