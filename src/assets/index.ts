42c48b27536fd37e2b0b5fe39c86411ff898cae2
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2023-06-03 21:44:22 +0400

    add assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,6 +1,23 @@
 import gptOmni from "./portfolioImages/gpt-omni.png"
 import kanban from "./portfolioImages/kanban.png"
+import twitter from "./portfolioImages/twitter.png"
+import dalle from "./portfolioImages/dalle.png"
+import portfolio from "./portfolioImages/portfolio.png"
 import gptOmniIcon from "./portfolioImages/gpt-omni-icon.svg"
 import kanbanIcon from "./portfolioImages/kanban-icon.svg"
+import twitterIcon from "./portfolioImages/twitter-icon.svg"
+import openAiIcon from "./portfolioImages/openAi-icon.svg"
+import logoIcon from "./logo.svg"
 
-export { gptOmni, kanban, gptOmniIcon, kanbanIcon }
+export {
+  gptOmni,
+  kanban,
+  portfolio,
+  gptOmniIcon,
+  kanbanIcon,
+  twitterIcon,
+  logoIcon,
+  openAiIcon,
+  dalle,
+  twitter
+}
